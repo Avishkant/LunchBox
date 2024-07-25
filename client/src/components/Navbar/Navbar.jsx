ba1e04a01233ac26e59ae9e54f8931895ff2da4b
mohitsen111 <mohitmahi1008@gmail.com>
2024-07-26 01:19:26 +0530

    logo changed

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,18 +3,19 @@ import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import ProfileAvatar from './Avatar'
 import NavbarMenu from './NavbarMenu'
-import logo from '../TiffinWalaLogo.png'
+// import logo from '../TiffinWalaLogo.png'
+// import logo from '../Lunch_Box.png'
 
 function NavbarLg({ name }) {
   return (
     <>
       <div className='w-full md:flex hidden justify-between shadow items-center py-3 lg:px-16 md:px-12 sm:px-8 px-2'>
         <Link to="/" className='flex gap-1.5 items-center'>
-          <img src={logo} className='h-[70px] object-contain' alt="" />
-          <h1 className='font-bold text-xl font-mono'>Tiffin Wala</h1>
+          <img src={`${process.env.PUBLIC_URL}/Lunch_Box.png`} className='h-[70px] object-contain' alt="Logo" />
+          <h1 className='font-bold text-xl font-mono'>Lunch Box</h1>
         </Link>
         <ul className='gap-4 flex items-center font-semibold '>
-          <li><Link to="/provider">Tiffin Providers</Link></li>
+          <li><Link to="/provider">Lunch Box</Link></li>
           {name ? <ProfileAvatar name={name} /> : <NavbarMenu />}
         </ul>
       </div>
@@ -27,7 +28,7 @@ function NavbarSm({ name }) {
     <>
       <div className='flex md:hidden px-4 py-2 shadow justify-between'>
         <Link to="/" className='flex items-center'>
-          <img src={logo} className='h-[50px] object-contain' alt="" />
+          <img src={`${process.env.PUBLIC_URL}/Lunch_Box.png`} className='h-[50px] object-contain' alt="" />
           <h1 className='font-bold text-xl font-mono'>Tiffin Wala</h1>
         </Link>
         <div className='flex items-center gap-3'>
